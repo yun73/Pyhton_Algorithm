597f1515658e1dfe809ebc95706207c5c1e3db91
lyh12qwaszx <lyh12qwaszx@gmail.com>
2023-08-07 01:28:43 +0900

    시험공부

--- solving_club/ws/List2/16311_special_sort.py
+++ solving_club/ws/List2/16311_special_sort.py
@@ -7,17 +7,16 @@ for tc in range(1, T + 1):
     for i in range(10):
         if i % 2 == 1:
             minidx = i
-            for j in range(i+1, N):
+            for j in range(i + 1, N):
                 if nums[minidx] > nums[j]:
                     minidx = j
             nums[i], nums[minidx] = nums[minidx], nums[i]
 
         else:
             maxidx = i
-            for j in range(i+1, N):
+            for j in range(i + 1, N):
                 if nums[maxidx] < nums[j]:
                     maxidx = j
             nums[i], nums[maxidx] = nums[maxidx], nums[i]
-
 
     print(f'#{tc}', *nums[0:10])
