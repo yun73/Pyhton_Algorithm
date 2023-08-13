cb59b4ff64a92982306ebb73190fde1b94969f0f
이윤형 <lyh12qwaszx@gmail.com>
2023-08-13 22:04:40 +0900

    시험공부

--- solving_club/ws/stack/16476_bracket.py
+++ solving_club/ws/stack/16476_bracket.py
@@ -3,22 +3,23 @@ for tc in range(1, T + 1):
     code = input()  # 주어진 입력
 
     # 하나씩 저장하며 확인할 스택
-    stack = []
+    stack = [0]*100
 
     # 정상적으로 짝 1, 비정상 0
     answer = 1
-
+    top = -1
     for c in code:
 
         if c == '(' or c == '{':
-            stack.append(c)
+            top += 1
+            stack[top] = c
 
         elif c == ')' or c == '}':
-            if len(stack) == 0:
+            if top == -1:
                 answer = 0
                 break
-
-            b = stack.pop()
+            top -= 1
+            b = stack[top+1]
             if (b != '(' and c == ')') or (b != '{' and c == '}'):
                 answer = 0
                 break
@@ -26,7 +27,41 @@ for tc in range(1, T + 1):
         else:
             continue
 
-    if len(stack) > 0:
+    if top != -1:
         answer = 0
 
     print(f'#{tc} {answer}')
+
+#  # ==============pop, append 안쓰고 해보기
+# T = int(input())
+# for tc in range(1, T + 1):
+#     code = input()  # 주어진 입력
+#
+#     # 하나씩 저장하며 확인할 스택
+#     stack = []
+#
+#     # 정상적으로 짝 1, 비정상 0
+#     answer = 1
+#
+#     for c in code:
+#
+#         if c == '(' or c == '{':
+#             stack.append(c)
+#
+#         elif c == ')' or c == '}':
+#             if len(stack) == 0:
+#                 answer = 0
+#                 break
+#
+#             b = stack.pop()
+#             if (b != '(' and c == ')') or (b != '{' and c == '}'):
+#                 answer = 0
+#                 break
+#
+#         else:
+#             continue
+#
+#     if len(stack) > 0:
+#         answer = 0
+#
+#     print(f'#{tc} {answer}')
