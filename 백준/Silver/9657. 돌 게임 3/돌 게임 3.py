a01c354f23ec474fddc7d74975a6dad9599b3db0
이윤형 <139411504+yun73@users.noreply.github.com>
2023-08-18 16:24:29 +0900

    [Silver III] Title: 돌 게임 3, Time: 44 ms, Memory: 31364 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,32 @@
+def re(N):
+    if dp[N] == 0:
+        for i in range(1,5):
+            if i == 2:
+                continue
+            if re(N-i) == -1:
+                dp[N] = 1
+                break
+        else:
+            dp[N] = -1
+    
+    return dp[N]
+
+
+N = int(input())
+dp = [0] * 1001
+
+dp[1] = 1  # 'SK'
+dp[2] = -1  # 'CY'
+dp[3] = 1
+dp[4] = 1
+dp[5] = 1
+dp[6] = 1
+dp[7] = -1
+dp[8] = 1
+dp[9] = -1
+
+re(N)
+if dp[N] == 1:
+    print('SK')
+else:
+    print('CY')
