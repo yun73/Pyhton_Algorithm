91ac3f9daa18e2ddda1a73614d6e0f34220529e5
이윤형 <139411504+yun73@users.noreply.github.com>
2023-08-23 12:15:01 +0900

    [D3] Title: 농작물 수확하기, Time: 202 ms, Memory: 61,720 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,24 @@
+'''
+IM 대비 문제 풀기
+'''
+# 농작물 수확
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    crop = [list(map(int,input())) for _ in range(N)]
+
+    all = 0
+    k = 0
+    i = N // 2
+    r = 0
+    while k <= N // 2:
+        for c in range(i-k,i+k+1):
+            all += crop[r][c]
+            all += crop[N-r-1][c]
+        r += 1
+        k += 1
+
+    for du in crop[N // 2]:
+        all -= du
+
+    print(f'#{tc} {all}')
