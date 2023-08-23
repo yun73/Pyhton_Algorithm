9cd42556269caa02ae95555e3fb22230998bb390
lyh12qwaszx <lyh12qwaszx@gmail.com>
2023-08-24 00:22:17 +0900

    0822 집

--- 백준풀기.py
+++ 백준풀기.py
@@ -1,23 +1,19 @@
 '''
 IM 대비 문제 풀기
 '''
-# 스위치 끄고 켜기
-# 1 : 켜짐 , 0 : 꺼짐
+# 수열
+# N 온도 측정 일수, K 합할 연속 일수
+N, K = map(int, input().split())
+# 온도 데이터
+temp = list(map(int, input().split()))
+K_sum = []
+# 처음 K일의 합을 미리 구하여 다음 합에 이용
+K_sum.append(sum(temp[:K]))
+for i in range(N-K):
+    # 순회가 진행 될 때 마다 K_sum의 top이 i = 0, top = 0 ==> i = 1, top = 1
+    # 이전 K일 동안의 합에 시작온도 빼고 하루 뒤의 온도 추가
+    K_sum.append(K_sum[i] - temp[i] + temp[K+i])
 
-# 스위치 개수, 100이하 양의 정수
-N = int(input(0))
-# 각 스위치의 상태
-switch = [-1] + list(map(int, input().split()))
-# 학생수
-num = int(input())
-for _ in range(num):
-    # 학생의 성별, 받은 수
-    # 1 : 남학생, 2 : 여학생
-    gender, s = map(int, input().split())
-    # 남학생은 스위치 번호가 자기가 받은 수의 배수이면, 그 스위치의 상태를 바꾼다.
-    # 여학생은 자기가 받은 수와 같은 번호 위치에서 좌우로 회문 판별 최대로 하고 바꾸기
-    if gender == 1: # 남자일 때
-        for i in range(s,8, s):
-            switch[i] = (switch[i]+1)%2
-    else: # 여자일 때
-    # 바꾸는거 mod 연산으로 하자
+print(max(K_sum))
+
+
