8b0e15d52914c6e78e17fe00b47404436d465e06
lyh12qwaszx <lyh12qwaszx@gmail.com>
2023-08-24 23:40:58 +0900

    0824 과제어케 풀어..

--- solving_club/hw/hw_ad_1_password_scan.py
+++ solving_club/hw/hw_ad_1_password_scan.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.stdin = open("input_code.txt", "r")
 
 pat = {
@@ -20,7 +19,7 @@ def hextobin(code):
     # 16진수를 2진수로 바꾸면 4배
     l = len(code) * 4
     # 배수
-    multi = l // 56
+
     # 16진수 문자열을 10진수로 바꾼값
     x = int(code, 16)
     # 컴퓨터는 우리가 바꾼 10진수인 dec를 2진수로 보고 있음
@@ -34,69 +33,91 @@ def hextobin(code):
         else:
             bin_string += '0'
 
-    result = ''
+    result = []
     i = l - 1
     while i > -1:
         if bin_string[i] == '1':  # 여기서부터 암호코드 끝점
-            result = bin_string[i - multi * 56 + 1: i + 1]
-            i -= multi * 56
+            for mul in range(1,30):
+                if i - mul * 56 +1 > -1:
+                    result.append((bin_string[i - mul * 56 + 1: i + 1], mul))
+                else:
+                    i -= 56
+                    break
         else:
             i -= 1
-
-    return result
+    result2 = set(result)
+    return result2
 
 
 T = int(input())
 for tc in range(1, T + 1):
     # N 세로크기, M 가로크기
     N, M = map(int, input().split())
-    arr = [input()[:M] for _ in range(N)]
+    first = [input()[:M] for _ in range(N)]
     # 주어진 암호코드 저장하는 곳
     codelist = []
+    arr = set(first)
     # 각 행에 코드가 있나 확인하고 있으면 코드 리스트에 넣어주나
     for r in arr:
-        if r != '0' * M:
-            for i in range(M, -1, -1):
-                if r[i] != 0:
-                    code += r[i]
-                else:
-                    if code != '':
-                        codelist.append(code[::-1])
-                    code = ''
+        #뒤에서부터 보면서
+        for i in range(M-1, -1, -1):
+            # 0이 아닌 값들이 나온 순간 코드 시작
+            if r[i] != '0':
+                if r[:i+1] not in codelist:
+                    # 내 앞에 있는 수들 전부다
+                    codelist.append(r[:i+1])
+
+                break
+
     total = 0
     # 코드 저장소 내에 있는 코드들 하나씩 꺼내서 다 바꾸고 암호검산 하자
     for code in codelist:
         # 2진수로 변환한 코드 얻기
-        binary = hextobin(code)
-        # 이진수 길이
-        bl = len(binary)
-        # 숫자 하나당 길이
-        interval = bl // 8
-        # 몇 배수 임??
-        mul = interval // 7
-        # 코드 하나당 7의배수의 길이를 가지고 총 8개의 숫자 있음
-        # 배수된 길이 만큼 끊어서 읽자
-        password = []
-        for i in range(0, bl, interval):
-            arrcode = ''
-            cnt = 1
-            # 숫자 하나에 해당하는 코드 순회
-            for j in range(i + 1, i + interval):
-                if binary[i - 1] == binary[i]:
-                    cnt += 1
+        result = hextobin(code)
+        for casee in result:
+            binary, mul = casee
+            # 이진수 길이
+            bl = len(binary)
+            # 숫자 하나당 길이
+            interval = bl // 8
+            # 코드 하나당 7의배수의 길이를 가지고 총 8개의 숫자 있음
+            # 배수된 길이 만큼 끊어서 읽자
+            password = []
+            for i in range(bl-interval, -1, -interval):
+                arrcode = ''
+                cnt = 1
+                # 숫자 하나에 해당하는 코드 순회
+                for j in range(i+1 , i + interval):
+                    if binary[j-1] == binary[j]:
+                        cnt += 1
+                        if j == i + interval - 1:
+                            arrcode += str(cnt // mul)
+                    else:
+                        arrcode += str(cnt // mul)
+                        cnt = 1
+                        if j == i + interval - 1:
+                            arrcode += str(cnt // mul)
+
+                if arrcode in pat.keys():
+                    password.append(pat[arrcode])
                 else:
-                    arrcode += str(cnt // mul)
-                    cnt = 1
+                    # 없으면 틀린거니까 다음거로 넘어가자
+                    break
 
-            password.append(pat[arrcode])
-        odd = 0
-        even = 0
-        for k in range(0, 8, 2):
-            odd += password[k]
-            if k+1 != 7:
-                even += password[k+1]
+                if len(password) == 8:
+                    passcode = password[::-1]
+                    odd = 0
+                    even = 0
+                    for k in range(0, 8, 2):
+                        odd += passcode[k]
+                        if k+1 != 7:
+                            even += passcode[k+1]
 
-        result = odd *3 +even +password[-1]
-        if result%10 == 0:
-            total += sum[result]
+                    cal = odd * 3 +even +passcode[-1]
 
+                    if cal%10 == 0:
+                        total += sum(passcode)
+
+                    password = []
+
+    print(f'#{tc} {total}')
--- 백준풀기.py
+++ 백준풀기.py
@@ -1,19 +1,8 @@
 '''
 IM 대비 문제 풀기
 '''
-# 수열
-# N 온도 측정 일수, K 합할 연속 일수
-N, K = map(int, input().split())
-# 온도 데이터
-temp = list(map(int, input().split()))
-K_sum = []
-# 처음 K일의 합을 미리 구하여 다음 합에 이용
-K_sum.append(sum(temp[:K]))
-for i in range(N-K):
-    # 순회가 진행 될 때 마다 K_sum의 top이 i = 0, top = 0 ==> i = 1, top = 1
-    # 이전 K일 동안의 합에 시작온도 빼고 하루 뒤의 온도 추가
-    K_sum.append(K_sum[i] - temp[i] + temp[K+i])
+# 창고 다각형
 
-print(max(K_sum))
+
 
 
