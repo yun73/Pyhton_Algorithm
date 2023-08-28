68b48f35c1b716799a8a17c725cea664294a2c3d
이윤형 <139411504+yun73@users.noreply.github.com>
2023-08-28 12:07:14 +0900

    [Silver III] Title: 소수 구하기, Time: 744 ms, Memory: 39068 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+# M 이상 N 이하인 소수 출력
+M, N = map(int, input().split())
+# 에라토스테네스의 체
+is_prime = [True] * 1000001
+is_prime[0] = False
+is_prime[1] = False
+for i in range(2,N):
+    if is_prime[i]:
+        j = 2
+        while i*j <= N:
+            is_prime[i*j] = False
+            j += 1
+
+for n in range(M,N+1):
+    if is_prime[n]:
+        print(n)
