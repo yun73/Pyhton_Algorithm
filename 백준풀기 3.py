5bd1fd2d7c9c5047866a0e6b865c675a95e5b446
lyh12qwaszx <lyh12qwaszx@gmail.com>
2023-09-04 22:19:53 +0900

    0904 스터디

--- 백준풀기 3.py
+++ 백준풀기 3.py
@@ -1,5 +1,7 @@
 # 스택
 # 정수를 저장하는 스택 구현
+import sys
+input = sys.stdin.readline
 
 # 명령의 수
 N = int(input())
@@ -9,6 +11,7 @@ for i in range(N):
     command = input()
     if 'push' in command:
         push, num = command.split(' ')
+        print(push, num)
         top += 1
         stack[top] = int(num)
     elif command == 'top':
@@ -32,3 +35,31 @@ for i in range(N):
         else:
             print(stack[top])
             top -= 1
+
+T = int(input())
+for tc in range(T):
+    N = int(input())
+
+    answer = 0
+
+    answer_l = []
+
+    count = 0
+
+    for i in range(1, N+1):
+        if i == 1:
+            answer = 0
+            answer_l.append(1)
+        if i == 2:
+            answer = 1
+            answer_l.append(2)
+        if i == 3:
+            answer = 3
+            answer_l.append(4)
+        if i >= 4:
+            count = sum(answer_l)
+            answer = count
+            answer_l.append(count)
+            answer_l.pop(0)
+
+    print(answer)
