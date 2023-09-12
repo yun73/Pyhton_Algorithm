dd1b128e9966cd7aeef54326ec328bd97da18310
이윤형 <139411504+yun73@users.noreply.github.com>
2023-09-12 23:44:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,18 @@
+# 요세푸스 문제
+# N 명이 원을 이루면서 앉아있다,
+# K 번째 사람 제거
+
+N, K = map(int, input().split())
+# 원형큐 생성 원형큐이므로 시작에 0추가
+q = [i for i in range(1, N + 1)]
+ans = []
+arr = [i for i in range(1, N + 1)]
+num = 0
+for i in range(N):
+    num += (K - 1)
+    if num >= len(arr):
+        num %= len(arr)
+    ans.append(str(arr[num]))
+    arr.pop(num)
+
+print("<", ', '.join(ans), ">", sep="")
