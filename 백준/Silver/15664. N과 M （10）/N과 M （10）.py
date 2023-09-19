0b7d7f62dac1f130dba5a74b1a103ad793203340
이윤형 <139411504+yun73@users.noreply.github.com>
2023-09-19 23:21:34 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,37 @@
+# N 과 M 끝내기
+
+# 자연수 N, M
+# N개의 자연수 중에서 M개를 고른 수열
+# 고른 수열은 비내림차순이어야 한다.
+# 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면,
+# 비내림차순이라고 한다.
+
+# s : 지금까지 선택한 개수
+# e : 선택할 최종 개수
+# li : 지금까지 만든 리스트
+def bt(s, e, li, before):
+    global perm
+
+    # 종료조건
+    if s == e:
+        if li not in perm:
+            perm.add(li)
+            print(li)
+        return
+
+    # 재귀호출
+    for i in range(before, N):
+        if not used[i]:
+            # 선택한 거 표시할 필요 없음
+            # 그리고 다음거 고르러가자
+            used[i] = True
+            bt(s+1,e,li + str(nums[i]) + ' ',i)
+            used[i] = False
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+used = [False] * N
+perm = set()
+li = ''
+bt(0,M,li,0) # 시작, 끝
