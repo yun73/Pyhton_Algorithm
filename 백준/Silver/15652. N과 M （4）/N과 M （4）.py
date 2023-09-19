66382284a39e68e6479239bc5ce8932d5aafb945
이윤형 <139411504+yun73@users.noreply.github.com>
2023-09-19 22:46:36 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,26 +1,32 @@
-# N 과 M
+# N 과 M 끝내기
 
-def perm(r,M,mc):
-    if r == M: # M개의 원소를 고르면
-        for i in range(M):
-            for j in range(1,N+1):
-                if nums[i][j] == 1:
-                    print(j, end = ' ')
-        print()
+# 자연수 N, M
+# 1 ~ N 중 M 개를 고른 수열
+# 같은 수를 여러번 골라도 된다
+# 고른 수열은 비내림차순이어야 한다.
+# 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면,
+# 비내림차순이라고 한다.
+
+# s : 지금까지 선택한 개수
+# e : 선택할 최종 개수
+# li : 지금까지 만든 리스트
+# before : 이전 값의 인덱스
+def bt(s, e, li,before):
+    # 종료조건
+    if s == e:
+        print(li)
         return
-    else:
-        for c in range(1,N+1):
-            if c >= mc: # 자기 자신보다 크거나 같은거에 대해서만
-                nums[r][c] = 1
-                mc = c
-                perm(r+1, M, mc)
-                nums[r][c] = 0 # 포함할 때 다 탐색하고 돌아와서
-                # 0으로 바꾸고 다음열 탐색 ex) 3 2> 11,12,13 탐색후
-                # 시작부분 1인거 다 끝나고 시작부분 2부터
+
+    # 재귀호출
+    # 이전거 보다 큰 거에서 탐색
+    for i in range(before, N):
+        # 선택한 거 표시할 필요 없음
+        # 그리고 다음거 고르러가자
+        bt(s+1,e,li + str(i+1) + ' ', i)
+
 
 N, M = map(int, input().split())
-# 1 부터 N 까지의 숫자중 중복 가능하게 M 개 골라야 하므로
-# 1~N 까지의 리스트 M 개 인 배열 생성
-nums = [[0]*(N+1) for _ in range(M)]
-# print(nums)
-perm(0,M,1)
+used = [False] * N
+# li = [0] * M
+li = ''
+bt(0,M,li,0) # 시작, 끝
