5173ee0cc71fd1e6d02efdbc2c7f86bc75022c3c
이윤형 <139411504+yun73@users.noreply.github.com>
2023-09-20 16:11:41 +0900

    [Silver II] Title: 꽃길, Time: 340 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -10,7 +10,7 @@
 
 
 def bt(now,End, price):
-    global visit
+    global visited
     global min_price
 
     # 종료조건 : 꽃 3개 다 심으면
@@ -27,34 +27,24 @@ def bt(now,End, price):
     # 가장자리 안되니까 제외하고 탐색
     for r in range(1,N-1):
         for c in range(1,N-1):
-            # 현재 위치에 꽃심기 못하면 다음위치 탐색
-            if is_possible[r][c]:
-                continue
             # 가능하다면
             # 일단 가격 계산
             now_price = field[r][c]
+            stack = [(r,c)]
             for dr,dc in [[1,0],[-1,0],[0,1],[0,-1]]:
                 nr,nc = r+dr, c +dc
-                if 0 <= nr < N and 0 <= nc < N:
-                    now_price += field[nr][nc]
-
-            # 해당 위치에 꽃을 심었을 때 다른 꽃이 심지 못하는 곳 체크하고
-            # 현재 위치에서 마름모 구간
-            for i in range(r-2,r+3):
-                for j in range(c-2,c+3):
-                    # 거리 계산 방법
-                    # abs|r - d| + abs|c - d| <= d
-                    if 0<=i<N and 0<=j<N and abs(i - r) + abs(j - c) <= 2:
-                            is_possible[i][j] += 1
-            # 다음 꽃 위치 심을 곳 찾으러 가자
-            bt(now+1, End, price + now_price)
-            # 갔다와서 초기화
-            for i in range(r-2,r+3):
-                for j in range(c-2,c+3):
-                    # 거리 계산 방법
-                    # abs|r - d| + abs|c - d| <= d
-                    if 0<=i<N and 0<=j<N and abs(i - r) + abs(j - c) <= 2:
-                        is_possible[i][j] -= 1
+                # 다른 꽃잎이 있으면
+                if visited[nr][nc]:
+                    break
+                now_price += field[nr][nc]
+                stack.append((nr,nc))
+             # 없으면
+            else:
+                for i,j in stack:
+                    visited[i][j] += 1
+                bt(now+1, End, price + now_price)
+                for i,j in stack:
+                    visited[i][j] -= 1
 
 
 # 꽃밭 크기
@@ -65,7 +55,7 @@ field = [list(map(int, input().split())) for _ in range(N)]
 # 꽃을 무조건 다 피게 해야하니까
 # 겹치는 경우는 아이에 보지 말자
 # 피면 안되는 위치 저장
-is_possible = [[1] * N] + [[1] + [0]*(N-2) + [0] for _ in range(N-2)] +[[0] * N]
+visited = [[0]*(N) for _ in range(N)]
 # 최소비용
 min_price = 200000
 # 꽃 심으러 가자
