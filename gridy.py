757ae3c3f668b613d72471f8bf1bfd8757b3680b
klica123456 <maruck3@naver.com>
2023-10-05 17:58:02 +0900

    1005 싸피

--- gridy.py
+++ gridy.py
@@ -1,42 +1,110 @@
 '''
-팀빌딩
+로봇
 
-- 개발자 : N 명
+- 이동 : 바라보는 방향 궤도를 따라(동, 서, 남, 북)
+- 로봇 이동 제어 명령
+    1. GO k: k는 1,2,3 중 하나, 현재 향한 방향으로 k칸 만큼 움직여라
+    2. Turn dir: dir은 left or right, 각각 왼쪽 오른쪽으로 90도 회전
 
-- 팀 : 반드시 개발자 2명
-    - 개발자 A, B
-        - (개발자 A와 개발자 B 사이에 존재하는 다른 개발자 수) × min(개발자 A의 능력치, 개발자 B의 능력치)
+- 궤도
+    - 0 : 궤도가 있어 로봇이 갈 수 있는 지점
+    - 1 : 궤도가 없어 로봇이 갈 수 없는 지점
 
-- 팀 빌딩에서 나 올 수 있는 팀 중 능력치의 최대값
+- 입력
+    - 세로길이 : M, 가로길이 : N / 둘 다 100 이하
+    - 궤도 서치상태 M줄
+    - 출발위치, 바라보는 방향
+    - 도착위치 바라보는 방향
+- 방향
+    - 동쪽 1, 서쪽 2, 남쪽 3, 북쪽 4
+    - 0 1 2 3
+    - di = [(0,1),(0,-1),(1,0),(-1,0)]
+    - di_dict = {0:1, 1:0 3 :4 , 4 :3}
 
-- A 개발자 위치를 a
-- B 개발자 위치를 b 라 할 때
-- 능력치가 최대가 되기 위해서는 최소값이 커야 하므로
-- 둘 중 작은 걸 옮기면서 큰걸 찾아가자
-두 위치의 능력치중 작은쪽의 인덱스를 1만큼 조작
+- 구해야 할 것
+    - 최단 명령
+        - 길이가 짧은지 x
+        - 명령 개수가 적어야 함
 
+    - 각 궤도에서 다른 방향으로 가기 위한 명령횟수를 구하고
+    - 명령수 가 적은 순으로 우선순위 큐에 넣자
 '''
+import heapq
+di = [(0,1),(0,-1),(1,0),(-1,0)]
+di_dict = {0:1, 1:0 , 2 :3 , 3 :2}
 
-N = int(input())
-X = list(map(int, input().split()))
-a = 0
-b = N-1
-max_ability = (b-a-1)*min(X[a], X[b])
-while a < b:
-    # 두 개발자 사이의 다른 개발자 수
-    p = b-a-1
-    # 작은 쪽 숫자의 인덱스 조작
-    if X[a] <= X[b]:
-        # 둘 중 최소 능격치
-        x = X[a]
-        a += 1
-    else:
-        x = X[b]
-        b -= 1
+def dijk(sr,sc,go,er,ec,eg):
+    # 우선순위 큐 생성
+    pq = []
+    INF = int(1e9)
+    # 누적 명령 횟수
+    command = [[[INF,0] for _ in range(N)] for _ in range(M)]
+    # 시작 지점 초기화
+    command[sr][sc][0] = 0
+    command[sr][sc][1] = go
+    if (sr,sc) == (er,ec):
+        if go == eg:
+            return 0
+        elif go == di_dict[eg]:
+            return 2
+        else:
+            return 1
+    heapq.heappush(pq,(0,sr,sc))
+    while pq:
+        # 우선순위 가장 높은(누적 명령어 가장 낮은) 위치 가져오기
+        acc_cmd, r, c = heapq.heappop(pq)
+        # 만약 이미 방문했거나 더 큰 누적치가 들어오면
+        if command[r][c][0] < acc_cmd:
+            continue
 
-    # 팀능력치 계산 후 갱신
-    ability = p * x
-    if max_ability < ability:
-        max_ability = ability
+        # 현재 위치에서 갈 수 있는 방향 탐색
+        for i in range(4):
+            for k in range(1,4):
+                nr,nc = r + k*di[i][0], c + k*di[i][1]
+                # 범위 내이고 궤도가 존재하는 0 이면
+                if 0<=nr<M and 0<=nc<N and not arr[nr][nc]:
+                    # 누적 명령 계산
+                    # 이전 방향과 같은 곳이면
+                    new_acc_cmd = acc_cmd+1
+                    # 현재 바라보는 방향과 갈 곳의 방향 에 의해 명령 개수 추가
+                    if i == command[r][c][1]:
+                        new_acc_cmd += 0
+                    # 현재 보는 방향과 반대방향일 때
+                    elif command[r][c][1] == di_dict[i]:
+                        new_acc_cmd += 2
+                    else:
+                        new_acc_cmd += 1
 
-print(max_ability)
+                    # 만약 종료 값을 찾으면
+                    if (nr,nc) == (er,ec):
+                        # 현재 바라보는 방향과 갈 곳의 방향 에 의해 명령 개수 추가
+                        if eg == i:
+                            new_acc_cmd += 0
+                        # 현재 보는 방향과 반대방향일 때
+                        elif eg == di_dict[i]:
+                            new_acc_cmd += 2
+                        else:
+                            new_acc_cmd += 1
+
+                    # 새로운 갱신 값이 기존 누적값보다 크면 넣지 말자
+                    if command[nr][nc][0] <= new_acc_cmd:
+                        continue
+                    # 새로운 최단 명령누적값 저장
+                    command[nr][nc][0] = new_acc_cmd
+                    # 보고 있는 방향 저장
+                    command[nr][nc][1] = i
+                    heapq.heappush(pq,(new_acc_cmd,nr,nc))
+
+                # 범위 밖이거나 중간에 궤도 없는 곳 나오면 해당 방향의 그 이후로는 보지 말자
+                else:
+                    break # for k
+
+    return command[er][ec][0]
+
+M, N = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(M)]
+sr,sc,sg = map(int, input().split())
+er,ec,eg = map(int, input().split())
+print(dijk(sr-1,sc-1,sg-1,er-1,ec-1,eg-1))
+
+
--- 백준2.py
+++ 백준2.py
@@ -1,24 +1,59 @@
 '''
-1
-- 테스트 케이스 받기
-    - 무한으로 받고 입력 예외시 브레이크
+신기한 키보드
 
-- (a*b)%c == (a%c*b%c)%c와 같아서
-111 % 3 == (11 * 10 + 1) %3
+- 키보드
+    - 버튼 3개
+        - 왼쪽 : 왼쪽으로 커서 한칸
+        - 오른쪽 : 오른쪽으로 커서 한칸
+        - 엔터 : 문자열을 컴퓨터에 전송히여 컴퓨터 화면에 출력
+    - LCD 창 1개
+        - 문자열 S
+        - 창 크기 : 문자열 S 와 정확하게 같음
+
+- 커서 : 문지열의 가장 왼쪽 글자에 위치
+    -  문자 위에 표시되고 해당하는걸 엔터 누르면 출력됨
+    - LCD 창 벗어나지 않음
+
+- 알파벳 순서로 출력, 키를 누르는 횟수의 최소값
+
+- 알파벳 순서로 출력해야 하므로
+    - a,b,c,.. 현재 찾아야할 알파벳을 알아야 함
+    - 문자열 길이 최대 50
+    - 찾은 문자는 빈칸이 됨
+    - 찾으러 앞쪽과 뒤쪽 모두 가봐야됨
+- 엔터키는 찾으면 무조건 누르는 거니까 일단 나중에 생각
+- 그러면 내가 지금 보는 인덱스와 찾아야할 문자의 인덱스들을 알아야 한다.
+- 문자가 모두 소문자 이므로 아스키 코드로
 '''
+word = {}
+# 소문자 a~z
+for i in range(97,123):
+    word[chr(i)] = []
+print(word)
 
-while True:
-    try:
-        n = int(input())
-    except:
-        break
-    i = 1
-    num = 0
+S = list(input())
+for i in range(len(S)):
+    # 해당하는 알파벳의 인덱스를 추가
+    word[S[i]].append(i)
+print(word)
+# 현재 커서 위치
+cursor = 0
+# 전체 클릭 횟수
+total = len(S)
+for i in range(97,123):
+    is_clear = [0] * len(word[chr(i)])
+    # 해당하는 문자를 다 출력할 때 까지 조작
+    while 0 not in is_clear:
+        # 현재 커서 위치와 가까운 걸 먼저
+        min_sub = int(1e9)
+        q = 0
+        for j in range(len(word[chr(i)])):
+            if is_clear[j]:
+                continue
+            if min_sub > abs(cursor - word[chr(i)][j]):
+                min_sub = abs(cursor - word[chr(i)][j])
+                q = word[chr(i)][j]
 
-    while True:
-        num = num*10 + 1
-        num %= n
-        if num == 0:
-            print(i)
-            break
-        i += 1
+        total += (cursor - q)
+        cursor = cursor - (cursor - q)
+
