67087b61485de925db4fbbdfa1ecba135e96170b
이윤형 <139411504+yun73@users.noreply.github.com>
2023-10-19 21:53:23 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 5840 ms, Memory: 693540 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -0,0 +1,31 @@
+'''
+최소값 찾기
+
+- N개의 수 : A1, A2, ..., AN
+- L
+- Di = A[i-L+1:i+1] 중 최소값
+
+- D 에 저장된 수 출력
+    - i ≤ 0 인 Ai는 무시하고 D를 구해야
+
+- 무조건 끝 범위는 1 씩 차이남
+- 겹치는 길이는 L 만큼
+
+
+'''
+
+import sys, heapq
+input = sys.stdin.readline
+
+N, L = map(int, input().split())
+A = [0]+list(map(int, input().split()))
+temp = []
+heapq.heappush(temp,(A[1],1))
+print(A[1], end=' ')
+for i in range(2,N+1):
+    heapq.heappush(temp, (A[i],i))
+    # 만약 최소값의 인덱스가 지금 확인 하는 범위 밖이면 빼내기
+    while temp[0][1] < i-L+1:
+        heapq.heappop(temp)
+
+    print(temp[0][0], end=' ')
