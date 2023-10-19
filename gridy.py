a474dea7b268c0c9625409e4256f7e0d7b16e586
lyh12qwaszx <lyh12qwaszx@gmail.com>
2023-10-20 00:17:10 +0900

    1019 집

--- gridy.py
+++ gridy.py
@@ -1,79 +1,29 @@
 '''
-거울 설치
+최소값 찾기
 
-- 집
-    - 문 : '#' : 2개
-    - 양면거울 : '!'
-        - 45도 기울어진 대각선 방향
-        - 즉 90 도 꺾는 거만 가능하다
-    - 벽 : '*'
-    - 빈공간 : '.'
+- N개의 수 : A1, A2, ..., AN
+- L
+- Di = A[i-L+1:i+1] 중 최소값
 
-- 설치해야할 거울의 최소 개수
-- 문부터 시작하여 다른 문을 향해 탐색한다
-- 각 위치에는 해당 위치까지 오는데의 최소 거울 개수를 기록해준다
-- 빛은 탐색을 일직선 방향으로 밖에 못한다.
-- 일직선 탐색시 거울 설치할 수 있는 곳 나오면 좌우로 최소 거울의 개수 추가
-- 근데 최소 거리여도 오는 방향이 총 4군데가 가능하다
-- 따라서 visited 4차원으로 만들어주자
+- D 에 저장된 수 출력
+    - i ≤ 0 인 Ai는 무시하고 D를 구해야
+
+- 무조건 끝 범위는 1 씩 차이남
+- 겹치는 길이는 L 만큼
 
 '''
-
 import sys, heapq
 input = sys.stdin.readline
 
-# 방향
-# 상하좌우
-di = {0:(-1,0),1:(1,0),2:(0,-1),3:(0,1)}
-op_di = {0:1,1:0,2:3,3:2}
+N, L = map(int, input().split())
+A = [0]+list(map(int, input().split()))
+temp = []
+heapq.heappush(temp,(A[1],1))
+print(A[1], end=' ')
+for i in range(2,N+1):
+    heapq.heappush(temp, (A[i],i))
+    # 만약 최소값의 인덱스가 지금 확인 하는 범위 밖이면 빼내기
+    while temp[0][1] < i-L+1:
+        heapq.heappop(temp)
 
-
-N = int(input())
-house = [list(input().strip()) for _ in range(N)]
-
-# 문 위치를 찾자
-i=0
-door={}
-for r in range(N):
-    for c in range(N):
-        if house[r][c] == '#':
-            door[i]=(r,c)
-            i+=1
-        if i ==2: break
-    if i ==2: break
-
-#
-def dijk(door):
-    INF =int(1e9)
-    visited = [[[INF]*4 for _ in range(N)] for _ in range(N)]
-    # 탐색할 우선순위 큐 생성
-    # 지금 거리까지 오는 최소 거울 개수,이전 방향,x,y
-    sr,sc = door[0][0], door[0][1]
-    pq = []
-    for i in range(4):
-        visited[sr][sc][i] = 0
-
-    while pq:
-        mirror, before, x, y = heapq.heappop(pq)
-
-        # 지금 방향 일직선으로 쭉 탐색
-        for dis in range(1,N):
-            nx,ny = x+dis*di[before], y+dis*di[before]
-            if 0<=nx<N and 0<=ny<N:
-                # 만약 벽이면 종료하고
-                if house[nx][ny] == '*':
-                    break
-                # 만약 빈칸이면 최소거울개수 그대로 받아서 넘어가고
-                if house[nx][ny] == '.':
-                    visited[nx][ny][before] = mirror
-                # 만약 거울이면 해당 위치에서 90도 방향으로 최소 거울 개수 1 추가해주고
-                if house[nx][ny] == '!':
-                    visited[nx][ny][before] = mirror+1
-
-
-
-
-
-
-
-dijk(door)
+    print(temp[0][0], end=' ')
