4a1b5fb200d610965d9333015460bfc3def95cf7
yun73 <lyh12qwaszx@gmail.com>
2023-10-23 17:56:52 +0900

    1023 싸피

--- gridy.py
+++ gridy.py
@@ -1,63 +1,36 @@
 '''
-리모컨
+앱
 
+- 앱 :
+    - 실행중 : 한 개, 화면에 보임
+    - 활성화 : 여러개, 화면에 보이지 않아도 메인 메모리에 직전의 상태 기록
 
-- 리모컨
-    - 버튼 : 0 ~ 9 숫자, + 와 -
-    - '+' : 보고있는 채널에서 +1
-    - '-' : 보고있는 채널에서 -1
-    - 채널 : 무한대
-    - 채널 0에서 -를 누른 경우에는 채널이 변하지 않고
+    - 비활성화 : 메모리 부족상태에서 새로운 앱 실행 시키기 위해 활성화 앱들 중 몇 개 선택하여 메모리로부터 삭제
 
-- 수빈이가 지금 이동하려고 하는 채널은 N이다
-- 어떤 버튼이 고장났는지 주어졌을 때
+- 앱 : N 개 , A1~AN
+    - 각각 mi 바이트 만큼 메모리 사용
+    - 비활성 화 후 다시 실행시 추가 비용 ci
 
+- 새로운 앱 B 실행 : M 바이트 메모리 필요
+- 활성화 상태인 A 중 몇개 비활성화 >> M 바이트 이상의 메모리 추가확보
+- ci의 합 최소
 
-- 안고장난 버튼으로 주어진 숫자와 가장 가깝게 한번에 이동할수 있다
+- m의 합들이 M 이상이 될 때 최소 비용 기록
+- 이전 앱에 최소값이 기록되어 있다면 그게 그 앱의 최소
 -
-- + 와 - 버튼은 1 씩만 채널을 변경할 수 있기 떄문에
+
 '''
-
-N = input()
-M = int(input())
-is_bk_btn =[0]*10
-if M != 0:
-    btn = list(map(int, input().split()))
-    for b in btn:
-        is_bk_btn[b] = 1
-# 현재 위치
-now_ch = '100'
-min_click = int(1e9)
-# 주어진 숫자랑 현재 채널 위치랑 같아질 때까지 진행행
-# 해당 숫자가 없으면 가장 가까운 숫자로 해야하는데
-# 각 위치 확일 할 때 그 시점의 최소값이 전체의 최소값으로 만들어줄지는 모름
-# 같은 자리 문자열을 만들고 결과값 숫자로 바꿔서 비교
-# 최소값 저장
-def bt(i,end,now,now_click):
-    global min_click
-    # 종료 조건
-    if i == end:
-        print(end)
-        click = i + abs((len(N) - len(str(int(now)))))
-        result = abs(int(N) - int(now)) + click
-        min_click = min(min_click, result)
-        return
+import sys
+input = sys.stdin.readline
 
 
-    # 만약 만들어야 하는 숫자가 안부서졌으면
-    # 부서졌으면 안부서진거 중에 클릭
-    for num in range(10):
-        if is_bk_btn[num]:
-            continue
-        bt(i+1,end,str(num)+now,)
+
+N, M = map(int, input().split())
+m = list(map(int, input().split()))
+c = list(map(int, input().split()))
+
+# dp[i][j] :
+dp = [[0]*N for _ in range(N)]
 
 
-if now_ch == N:
-    print(0)
-elif M == 0:
-    print(min(len(N),abs(int(N)-100)))
-elif M == 10:
-    print(abs(int(N)-int(now_ch)))
-else:
-    bt(0,len(N)+1,'',0)
-    print(min_click)
+print(min(dp))
--- 백준2.py
+++ 백준2.py
@@ -1,112 +1,65 @@
 '''
-거의 최단 경로
+확장 게임
 
+- 땅 : N x M
+
+- 각 플레이어는 하나 이상의 성을 가짐
+
+- 성 확장 : 플레이어 i는 자신이 있는 곳에서 Si 만큼 이동할 수 있는 모든 칸에 성을 동시에 만듦
+- 상하좌우 가능
+- 턴 돌아가면서 확장
+- 모든 플레이어 확장 풀가능하면 게임 끝
+
+- 턴마다 내가가지고 있는 성의 위치에서 bfs 를 si칸 만큼 탐색 가능
+- 턴마다 bfs 탐색을 하면 시초날 거 같은데
 '''
-
-
-'''
-거울 설치
-
-- 집
-    - 문 : '#' : 2개
-    - 양면거울 : '!'
-        - 45도 기울어진 대각선 방향
-        - 즉 90 도 꺾는 거만 가능하다
-    - 벽 : '*'
-    - 빈공간 : '.'
-
-- 설치해야할 거울의 최소 개수
-- 문부터 시작하여 다른 문을 향해 탐색한다
-- 각 위치에는 해당 위치까지 오는데의 최소 거울 개수를 기록해준다
-- 빛은 탐색을 일직선 방향으로 밖에 못한다.
-- 일직선 탐색시 거울 설치할 수 있는 곳 나오면 좌우로 최소 거울의 개수 추가
-- 근데 최소 거리여도 오는 방향이 총 4군데가 가능하다
-- 따라서 visited 4차원으로 만들어주자
-
-'''
-
-import sys, heapq
+import sys
 from collections import deque
 input = sys.stdin.readline
 
-# 방향
-# 상하좌우
-di = [(-1,0),(1,0),(0,-1),(0,1)]
-op_di = {0:1,1:0,2:3,3:2}
-
 def is_valid(x,y):
-    if 0<=x<N and 0<=y<N:
+    if 0<=x<N and 0 <=y<M:
         return True
     return False
 
-N = int(input())
-house = [list(input().strip()) for _ in range(N)]
+def bfs(player):
 
-# 문 위치를 찾자
-i=0
-door={}
+    visited = [[0]*M for _ in range(N)]
+    count = [[0]*(P+1)]
+    for p in range(1,P+1):
+        new_player = deque()
+        # 현재 가지고 있는 영역을 다 탐색할 때 까지 반복
+        while player[p]:
+            x,y = player[p].popleft()
+            count[p] += 1
+
+            for dx,dy in ((),(),(),()):
+                for dis in range(S[p],0,-1):
+                    nx,ny = x+dx*dis, y+dy*dis
+                    if is_valid(nx,ny) and not visited[nx][ny]:
+                        visited[nx][ny] = p
+
+
+
+
+
+        else:
+            player[p] = new_player
+
+
+
+
+N,M,P = map(int,input().split())
+S = [0] + list(map(int, input().split()))
+land = [list(input()) for _ in range(N)]
+
+player = [deque([])*(P+1)]
 for r in range(N):
-    for c in range(N):
-        if house[r][c] == '#':
-            door[i]=(r,c)
-            i+=1
-        if i ==2: break
-    if i ==2: break
+    for c in range(M):
+        if land[r][c] != '#' and land[r][c] != '.':
+            player[int(land[r][c])].append((r,c))
 
-#
-def bfs(door):
-    INF = int(1e9)
-    visited = [[[INF]*4 for _ in range(N)] for _ in range(N)]
-    # 탐색할 우선순위 큐 생성
-    # 지금 거리까지 오는 최소 거울 개수,이전 방향,x,y
-    sr,sc = door[0][0], door[0][1]
-    end_x, end_y = door[1][0], door[1][1]
-    pq = deque([])
-    for i in range(4):
-        visited[sr][sc][i] = 0
-        pq.append((0,i,sr,sc))
+bfs(player)
 
-    while pq:
-        mirror, before, x, y = pq.popleft()
-        # if (x,y) == (end_x,end_y):
-        #     return mirror
-        # 지금 방향 일직선으로 쭉 탐색
-        for dis in range(N):
-            nx,ny = x+dis*di[before][0], y+dis*di[before][1]
-            # 경계 밖이면 종료
-            if not is_valid(nx,ny):
-                break
-            # 만약 벽이면 종료하고
-            if house[nx][ny] == '*':
-                break
-            # 만약 빈칸이면 최소거울개수 그대로 받아서 넘어가고
-            if visited[nx][ny][before] < mirror:
-                continue
-            visited[nx][ny][before] = mirror
 
-            # 만약 거울이면 해당 위치에서 90도 방향으로 최소 거울 개수 1 추가해주고
-            if house[nx][ny] == '!':
-                for d in range(4):
-                    # 이전과 같은 방향, 그 반대 방향일때는 제외 90도만 탐색
-                    if d == before or d == op_di[before]:
-                        continue
-                    nnx,nny = nx+di[d][0],ny+di[d][1]
-                    if is_valid(nnx,nny) and house[nnx][nny] != '*':
-                        if visited[nnx][nny][d] > mirror:
-                            visited[nnx][nny][d] = mirror+1
-                            pq.append((mirror+1,d,nnx,nny))
 
-    # 디버깅 코드=========================================================
-    # for t in range(4):
-    #     for r in visited:
-    #         for c in r:
-    #             if c[t] == INF:
-    #                 print('.', end =' ')
-    #                 continue
-    #             print(c[t], end=' ')
-    #         print()
-    #     print(f'{t}===============')
-    #=====================================================================
-    return min(visited[end_x][end_y])
-
-print(bfs(door))
