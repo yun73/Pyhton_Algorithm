7159869e5a29b949513f59680f95c6c9faaa80f7
lyh12qwaszx <lyh12qwaszx@gmail.com>
2023-10-26 00:47:53 +0900

    1024 집

--- gridy.py
+++ gridy.py
@@ -1,94 +1,57 @@
 '''
-배열에서 이동
+토마토
 
-- nxn 짜리 배열
-- 1,1 >> n,n
-
-- 지나가는 경로의 숫자들 중 최대값과 최소값의 차이가 최소일 때 그 값 출력
-
-- 일단 bfs를 이용한 경로 탐색이다
-- 근데 이제 그 경로의 숫자들의 최대, 최소의 차가 최소가 되어야 한다
-- 모든 케이스를 다 확인하기에는 시초
-- 탐색 방향은 상하좌우, visited는 한번만 해줘도 됨
-
-- 0 <= 최대 - 최소 <= 200
-- 생각 1. 이 범위를 이분탐색?
-
-- 생각 2. 가능한 숫자 범위를 이분 탐색
-    - 시작점 과 끝점이 1과 5 이면 (1,5) 로 시작
-        - 만약 해당 숫자 범위 안에 있는 칸으로만 가서 종료점에 도달한다면
-    - 최소값 후보와, 최대 값 후보를 정하자
-    - 예를 들어 최소 0 최대 8이면
-    최소 값 후보는 0~1 최대값 후보는 5~8
-
-- 아니 그냥
+- 현재 토마토 익으면 하루 뒤에 상하좌우 익게 한다
 
 '''
 import sys
 from collections import deque
 input = sys.stdin.readline
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
-def bfs(mid):
-    # 최대와 최소의 차이가 mid 이므로
-    # 최소 값인 i 부터 max_num-mid+1 즉 숫자들의 범위가 (i,i+mid) 인 경우에만 탐색해보면 됨
-    for i in range(min_num, max_num-mid+1):
-        s,e = i, i + mid
-        #  만약 시작점과 끝점이 해당 범위 안에 없으면 건너 뛰기
-        if not (s<= arr[0][0] <= e and s<= arr[n-1][n-1] <= e):
+def bfs(ripe):
+    global visited
+
+    new_ripe = deque()
+    while ripe:
+        x,y = ripe.popleft()
+        for i in range(4):
+            nx,ny = x + dx[i], y+dy[i]
+            if 0<=nx<N and 0<=ny<M and not visited[nx][ny] and box[nx][ny] == 0:
+                visited[nx][ny] = 1
+                new_ripe.append((nx,ny))
+
+    return new_ripe
+
+M,N = map(int, input().split())
+box = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+# 익은 토마토가 있는칸 모두 조사
+ripe = deque()
+cnt = 0
+for r in range(N):
+    for c in range(M):
+        if box[r][c] == -1:
             continue
+        if box[r][c] == 1:
+            ripe.append((r, c))
+            visited[r][c] = 1
+        cnt += 1
 
-        visited = [[0]*n for _ in range(n)]
-        q = deque()
-        q.append((0,0))
-        visited[0][0] = 1
-        while q:
-            x,y = q.popleft()
-            # 종료지점 찾으면 가능한거니까 True 반환
-            if (x,y)==(n-1,n-1):
-                return True
-            for dx,dy in ((1,0),(0,1),(-1,0),(0,-1)):
-                nx,ny = x + dx, y + dy
-                if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and s<= arr[nx][ny] <= e:
-                    q.append((nx,ny))
-                    visited[nx][ny] = 1
+
+if not ripe:
+    print(-1)
+else:
+    cnt -= len(ripe)
+    day = 0
+    while cnt>0:
+        if not ripe:
+            break
+        ripe = bfs(ripe)
+        cnt -= len(ripe)
+        day += 1
+    if cnt > 0:
+        print(-1)
     else:
-        return False
-
-
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-# for line in arr:
-#     print(*line)
-
-max_num = 0
-min_num = 201
-for r in range(n):
-    for c in range(n):
-        if max_num < arr[r][c]:
-            max_num = arr[r][c]
-        if min_num > arr[r][c]:
-            min_num = arr[r][c]
-
-# print(max_num,min_num)
-# 최대 최소 차이의 최소 = 0 , 최대 = max_num-min_num
-left,right = 0, max_num - min_num
-min_mid = 300
-while left <= right:
-    mid = (left + right)//2
-    # print(mid)
-    # 만약 이걸로 돌 수 있다면
-    if bfs(mid):
-        min_mid = min(min_mid,mid)
-        # 더 작은 최소 값으로 해보자
-        right = mid - 1
-    # 안된다면
-    else:
-        # 더 큰 값으로 해보자
-        left = mid + 1
-
-print(min_mid)
-
-
-
-
-
+        print(day)
--- 백준 스터디.py
+++ 백준 스터디.py
@@ -1,10 +1,2 @@
-N = int(input())
-for M in range(N//2,N):
-    part_sum = 0
-    for m in str(M):
-        part_sum += int(m)
-    if M + part_sum == N:
-        print(M)
-        break
-else:
-    print(0)
+A = [[1001]*3 for _ in range(3)]
+print(A//1000)
