e6558caacde7804767a88397799f98a6714ed4c1
이윤형 <139411504+yun73@users.noreply.github.com>
2023-10-30 12:10:26 +0900

    [Silver II] Title: 이항 계수 2, Time: 496 ms, Memory: 57008 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,32 @@
+'''
+이항계수
+(n \ k) = nCk = n!/(n-k)!k!
+
+(n \ k) = (n \ n-k)
+(n \ k) = (n -1 \ k) + (n-1\k-1)
+k = 0~n까지 sum(n\k) = 2^n
+
+'''
+
+def BC(n,k):
+    global visited
+    if visited[n][k]:
+        return visited[n][k]
+
+    if k == 0 or n == k:
+        visited[n][k] = 1
+        return visited[n][k]
+
+    if k == 1:
+        visited[n][k] = n
+        return visited[n][k]
+
+    visited[n][k] = BC(n-1,k) + BC(n-1,k-1)
+    return visited[n][k]
+
+
+
+N, K = map(int, input().split())
+visited = [[0]*(K+1) for _ in range(N+1)]
+BC(N,K)
+print(visited[N][K]%10007)
