6a867debffe63ee6359db8b95ba6a8b13369f640
이윤형 <lyh12qwaszx@gmail.com>
2023-10-31 17:57:24 +0900

    1031

--- 백준2.py
+++ 백준2.py
@@ -1,68 +1,45 @@
 '''
-확장 게임
+LCS
 
-- 땅 : N x M
+LCS(Longest Common Subsequence, 최장 공통 부분 수열)
 
-- 각 플레이어는 하나 이상의 성을 가짐
+문자의 순서가 바뀌지 않는 가장 긴 공통 부분 수열을 구하는문제
+- 비크마스킹으로 모든 겨웅를 하면 무조건 시간 초과
+- python 으로 2초 대략 4천만번 계산 가능
+- dp로 1000*1000 을 계산하면 100만 각 자리당 40번 까지 연산 가능하다 치자
 
-- 성 확장 : 플레이어 i는 자신이 있는 곳에서 Si 만큼 이동할 수 있는 모든 칸에 성을 동시에 만듦
-- 상하좌우 가능
-- 턴 돌아가면서 확장
-- 모든 플레이어 확장 풀가능하면 게임 끝
-
-- 턴마다 내가가지고 있는 성의 위치에서 bfs 를 si칸 만큼 탐색 가능
-- 턴마다 bfs 탐색을 하면 시초날 거 같은데
+- 가장 긴 부분수열부터 조사해야 한다.
+- 첫 문자열의 처음 부분부터 조사할 때 부터 차례대로
+dp[i][j] = i번째부터 시작했을 때의 최대 부분 수열 길이
+앞에거에 영향을 받으면서 가자
 '''
-import sys
-from collections import deque
 
-input = sys.stdin.readline
+first = input()
+second = input()
+if len(first) <= len(second):
+    short = first
+    long = second
+else:
+    short = second
+    long = first
 
-def is_valid(x, y):
-    if 0 <= x < N and 0 <= y < M:
-        return True
-    return False
+dp = [[0]*len(long) for _ in range(len(short))]
 
-def bfs(now, player):
-    global count
+for i in range(len(short)):
+    if short[i] != long[0]:
+        continue
+    dp[i][0] = 1
 
-    new_castle = deque()
-    while now:
-        x,y = now.popleft()
-        for dx, dy in ((1, 0), (-1, 0), (0, -1), (0, 1)):
-            nx, ny = x + dx, y + dy
-            if is_valid(nx, ny) and not visited[nx][ny] and land[nx][ny] == '.':
-                new_castle.append((nx, ny))
-                visited[nx][ny] = player
-                count[player] += 1
+for i in range(len(short)):
+    for j in range(len(long)):
+        dp[i][j] =
 
-    return new_castle
-
-
-N, M, P = map(int, input().split())
-S = [0] + list(map(int, input().split()))
-land = [list(input()) for _ in range(N)]
-visited = [[0]*M for _ in range(N)]
-count = [0] * (P + 1)
-castle = [deque() for _ in range(P+1)]
-for r in range(N):
-    for c in range(M):
-        if land[r][c] != '#' and land[r][c] != '.':
-            p = int(land[r][c])
-            castle[p].append((r,c))
-            visited[r][c] = p
-            count[p] += 1
-
-res = 1
-while res:
-    res = 0
-    for player in range(1,P+1):
-        for dis in range(1,S[player]+1):
-            if not castle[player]:
-                break
-            castle[player] = bfs(castle[player],player)
-        else:
-            res = 1
-
-count.pop(0)
-print(*count)
+'''
+      A C A Y K P
+K     0 0 0 0 1 1  
+AK    1 1 1 1 2 2
+CAK   1 2 2 2 3 3
+PCAK  1 2 2 2 3 3 
+APCAK 2 3 4 4 4 4
+CAPCAK2 3 4 4 4 4 
+'''
