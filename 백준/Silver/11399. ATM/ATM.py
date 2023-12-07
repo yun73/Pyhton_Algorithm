d1f56f485f9daa8d0ea8498fdcbe081aadbc70b0
이윤형 <139411504+yun73@users.noreply.github.com>
2023-12-07 16:11:07 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,16 @@
+'''
+각 사람이 돈을 인출하는데 필요한 시간은
+앞 사람의 일출 시간에 영향을 받으므로
+각 차례의 앞에는 자기보다 같거나 작은 수들만 있어야 한다
+'''
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+times = sorted(list(map(int, input().split())))
+
+res = 0
+for i in range(len(times)):
+    res += times[i] * (len(times)-i)
+
+print(res)
