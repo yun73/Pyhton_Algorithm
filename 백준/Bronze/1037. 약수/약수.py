3c2b87ceba1a1d810f16afbec2be87977a3a3639
이윤형 <139411504+yun73@users.noreply.github.com>
2023-12-23 21:46:03 +0900

    [Bronze I] Title: 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -4,9 +4,6 @@ input = sys.stdin.readline
 cnt = int(input())
 real = sorted(list(map(int, input().split())))
 
-if cnt % 2:
-    N = real[cnt//2]**2
-else:
-    N = real[0] * real[-1]
+N = real[0] * real[-1]
 
 print(N)
