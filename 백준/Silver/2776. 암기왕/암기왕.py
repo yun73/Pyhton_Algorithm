1b3e17f8a2bb7351cf5dacc90f7dc018a4b410f7
이윤형 <139411504+yun73@users.noreply.github.com>
2024-03-18 05:54:09 +0900

    [Silver IV] Title: 암기왕, Time: 1508 ms, Memory: 187272 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,21 @@
+
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    note1 = list(map(int, input().split()))
+    M = int(input())
+    note2 = list(map(int, input().split()))
+
+    note1 = set(note1)
+
+    for num in note2:
+        if num in note1:
+            print(1)
+        else:
+            print(0)
+           
+        
