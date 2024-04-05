775e87904be61203d31e7ab92797ac68e7232743
이윤형 <139411504+yun73@users.noreply.github.com>
2024-04-05 09:52:58 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,46 +1,12 @@
-def len_to_bin(len_s):
-    
-    # 길이 len_s 를 이진수로 변환
-    res = bin(len_s)[2:]
-
-    return res
-
-
-def delete_zero(new_s,c):
-    global cnt_zero
-    
-    arr = ''
-    
-    for i in range(c):
-        if new_s[i] == '0':
-            cnt_zero += 1
-        else:
-            arr += new_s[i]
-    # 현재 이진수의 0 제외한 수 리턴
-    
-    return arr
-
-
-def solution(s):
-    answer = []
-     
+def solution(s):  
     cnt_zero = 0
     cnt = 0
     new_s = s
     while len(new_s) != 1: # s 가 1이 될 때까지
-        now_s = ''
-        
-        for i in range(len(new_s)):
-            if new_s[i] == '0':
-                cnt_zero += 1
-            else:
-                now_s += new_s[i]
-            
-        new_s = len_to_bin(len(now_s))
+
+        num = new_s.count('0')
+        cnt_zero += num
+        new_s = bin(len(new_s)-num)[2:]
         cnt += 1
     
-    
-    answer.append(cnt)
-    answer.append(cnt_zero)
-    
-    return answer
+    return cnt,cnt_zero
