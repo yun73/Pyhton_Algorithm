e7ff026c85874f16e10ca5ad33ed8d741e777c58
이윤형 <139411504+yun73@users.noreply.github.com>
2024-04-10 18:41:15 +0900

    [Silver II] Title: 결혼식, Time: 476 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -0,0 +1,17 @@
+N = int(input())
+M = int(input())
+friends = [[] for _ in range(N+1)]
+set_friends = set()
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    friends[a].append(b)
+    friends[b].append(a)
+
+for first in friends[1]:
+    set_friends.add(first)
+    for second in friends[first]:
+        if second != 1 and second not in set_friends:
+            set_friends.add(second)
+
+print(len(list(set_friends)))
