7c8ff3b9f0b0643a273fe96576c1a6a982068994
이윤형 <139411504+yun73@users.noreply.github.com>
2024-04-14 23:44:53 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 76 ms, Memory: 34488 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -13,21 +13,24 @@ h : 1 ~ 1,000,000,000
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-H = [0] * N
-for i in range(N):
-    H[i] = int(input())
+def solve():
+    N = int(input())
+    H = [0] * N
+    for i in range(N):
+        H[i] = int(input())
 
-left = []
-cnt = 0
-for height in H:
-    # 지금 빌딩 앞에 빌딩이 있고,마지막에 추가된 값이 나보다 작거나 같다면
-    while left and left[-1] <= height: 
-        # 어짜피 이 이후는 못보니까 제거
-        left.pop()
-    # 현재 나를 볼 수 있는 건물 개수 추가
-    cnt += len(left)
-    # 현재 빌딩 추가
-    left.append(height)
+    left = []
+    cnt = 0
+    for height in H:
+        # 지금 빌딩 앞에 빌딩이 있고,마지막에 추가된 값이 나보다 작거나 같다면
+        while left and left[-1] <= height:
+            # 어짜피 이 이후는 못보니까 제거
+            left.pop()
+        # 현재 나를 볼 수 있는 건물 개수 추가
+        cnt += len(left)
+        # 현재 빌딩 추가
+        left.append(height)
 
-print(cnt)
+    return cnt
+
+print(solve())
