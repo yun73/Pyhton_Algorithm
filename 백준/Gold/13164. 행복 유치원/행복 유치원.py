49e6140fb0a5919d5525ed457bd775183c9f8d0f
이윤형 <139411504+yun73@users.noreply.github.com>
2024-04-16 23:54:16 +0900

    [Gold V] Title: 행복 유치원, Time: 232 ms, Memory: 66600 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -4,11 +4,8 @@ input = sys.stdin.readline
 def solve():
     N, K = map(int,input().split())
     students = list(map(int, input().split()))
-    group = [0]*(N-1)
-    for i in range(1,N):
-        group[i-1] = students[i]-students[i-1]
+    group = [students[i+1]-students[i] for i in range(len(students)-1)]
     group.sort()
-    # print(group)
     cost = sum(group[:N-1-(K-1)]) # 키 차이 큰 얘들을 혼자인 조가 되게
     return cost
 
